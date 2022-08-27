6c8e85ca66d4848e658dc37307a4f9b07148537b
Dyogo19 <dytobrabo@gmail.com>
2022-08-26 22:12:51 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+let nome = 'Dyogo';
+let idade = 17;
+let ishome = true;
+const conjuntoNumber = [23, 23];
+const conjuntoString = ['Jorge', 'Dyogo'];
+const conjuntoDinamico = ['Dyogo', 17];
+
+// function exibirNome() {
+//   console.log('Macarrao');
+// }
+// exibirNome();
+
+function exibirNome(idade) {
+  console.log(`macarrao, que tem ${idade} anos`);
+}
+exibirNome(30);
+
+console.log(nome);
+console.log(idade);
+console.log(ishome);
+console.log(conjuntoNumber);
+console.log(conjuntoString);
+console.log(conjuntoDinamico);
