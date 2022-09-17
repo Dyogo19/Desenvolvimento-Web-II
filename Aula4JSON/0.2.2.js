96b92af65ba666cd1ec42d64a7935debc5bcc925
Dyogo19 <104871305+Dyogo19@users.noreply.github.com>
2022-09-17 01:23:08 -0300

    3 files added, 2 deleted

---
+++ Aula4JSON/0.2.2.js
@@ -0,0 +1,43 @@
+const usuario = [
+  {
+  nome : 'Jorge' , 
+  idade : 17 , 
+  endereco : 'Alvorada City' , 
+  avaliacoes: [
+    {
+      nome : 'Dyogo' , 
+      comentario : 'Engraçado' , 
+      nome : 'Dyto' , 
+      comentario : 'Estiloso' 
+    }
+  ]
+} , 
+{
+  nome : 'Dyogo' , 
+  idade : 17 , 
+  endereco : 'Alvorada City' ,
+  avaliacoes: [
+   {
+    nome : 'Jorge' , 
+    comentario : 'Engraçado' , 
+    nome : 'Dyto' , 
+    comentario : 'Estiloso'  
+   }
+  ]
+} ,
+{
+  nome : 'Dyto' , 
+  idade : 17 , 
+  endereco : 'Alvorada City' , 
+  avaliacoes: [
+    {
+      nome : 'Jorge' , 
+      comentario : 'Engraçado' ,
+      nome : 'Dyogo' , 
+      comentario : 'Engraçado'
+    } 
+  ]
+}
+]  
+
+console.log(usuario);
