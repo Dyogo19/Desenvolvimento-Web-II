621f38d8d3a6a27ce2d075e40d340df286112622
Dyogo19 <104871305+Dyogo19@users.noreply.github.com>
2022-10-28 22:30:07 -0300

    5 files added, 3 deleted

---
+++ Aula10 1.7.1 react/index.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+
+const filmes = [
+  {
+    nome: "O Senhor do Aneis: A Sociedade do Anel",
+    lancamento: 2002,
+    oscar: true,
+    diretores: ["Peter Jackson"],
+    generos: ["Fantasia", "Aventura"]
+  },
+  {
+    nome: "Harry Potter e a Pedra Filosofal",
+    lancamento: 2001,
+    oscar: true,
+    diretores: ["Chris Columbus"],
+    generos: ["Fantasia"]
+  },
+  {
+    nome: "Matrix",
+    oscar: true,
+    lancamento: 1999,
+    diretores: ["Lana Wachowski", "Lilly Wachowski"],
+    generos: ["Ação", "Ficção Cientifica"]
+  },
+  {
+    nome: "Meninas Malvadas",
+    oscar: false,
+    lancamento: 2004,
+    diretores: ["Mark Waters"],
+    generos: ["Comédia"]
+  }
+];
+
+const filmao = filmes
+  .filter((filme) => filme.oscar === true)
+  .map((filme) => filme.nome)
+  .map((nome) => <h2>{nome}</h2>);
+
+export default function App() {
+  return <div>{filmao}</div>;
+}
